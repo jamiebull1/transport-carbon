cf8023bbea993fcd644fa108429d140a146354ee
jamiebull1 <jamiebull1@gmail.com>
2014-01-15 19:52:34 +0000

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 transport-carbon
 ================
 
-Package for calculating the carbon footprint of transport
+This is a start at creating an open source Python package for calculating the carbon footprint of transport (UK-focused to begin with).
+
+##Distances
+The first step is calculating travel distances. Planned modes are:
+
+###Air
+[x] Working prototype completed as distance.air_distance()
+[x] End to end unit test
+[x] Passes end to end unit test
+
+###Road
+[x] Working prototype completed as `distance.road_distance()`
+[x] End to end unit test
+[x] Passes end to end unit test
+
+###Rail
+[ ] Working prototype completed as `distance.rail_distance()`
+[x] End to end unit test
+[ ] Passes end to end unit test (fails on many options and so far no obvious pattern)
+
+###Sea
+[ ] Working prototype completed as `distance.sea_distance()` (a very sketchy prototype is under development based on waypoints and Dijkstra's shortest path algorithm)
+[ ] Create a grid over the seas and oceans and including all known ports
+[ ] Find a non-directional graphing package which implements a fast version of Dijkstra's shortest path algorithm
+[ ] End to end unit test
+[ ] Passes end to end unit test
+
+##Carbon Intensity
+The second task will be fetching carbon intensity. The plan is to find an API to serve carbon intensity data from Defra or to create one ourselves.
+
+##Contributing
+Please contact Jamie Bull at jamie.bull@oco-carbon.com if you would like to assist in developing this package
---
+++ src/distance.py
@@ -0,0 +1,107 @@
+'''
+Created on 9 Jan 2014
+
+@author: Jamie
+'''
+import math
+import requests
+import re
+
+from pygeocoder import Geocoder
+
+import stations
+from g_directions import GoogleDirections
+
+CHAINS_PER_MILE = 0.0125
+KM_PER_MILE = 1.6093
+
+EARTH_RADIUS = 6378137     # earth radius in meters
+AVIATION_DISTANCE_UPLIFT = 0.09
+
+def air_distance(origin, destination, units='km'):
+    ''' Uses great circle distance and an uplift factor of 9% following
+    Defra's guidance '''
+    latlng_a = Geocoder.geocode(origin).coordinates
+    latlng_b = Geocoder.geocode(destination).coordinates
+    
+    dist = great_circle_distance(latlng_a, latlng_b)
+
+    if units == 'km':
+        dist = dist / 1000.0
+    elif units == 'miles':
+        dist = dist / 1000.0 / KM_PER_MILE
+    else:
+        raise Exception('%s is not a valid unit system. Use "km" or "miles"' % units)
+
+    return dist * (1 + AVIATION_DISTANCE_UPLIFT)
+
+def road_distance(origin, destination, units='km'):
+    ''' Uses the Google Directions API '''
+    gd = GoogleDirections()
+    dist = gd.query(origin, destination).distance
+    if units == 'km':
+        dist = dist / 1000.0
+    elif units == 'miles':
+        dist = dist / 1000.0 / KM_PER_MILE
+    else:
+        raise Exception('%s is not a valid unit system. Use "km" or "miles"' % units)
+    
+    return dist
+    
+def rail_distance(origin, destination, units='km'):
+    ''' Uses the site railmiles.org as an unofficial API. It's very shaky so would
+    like to find a better source to use '''
+    origin = stations.closest(origin)
+    destination = stations.closest(destination)
+
+    query = {'origin': origin,
+             'destination': destination,
+             'type': 'text',
+             'shortestroutes': 'on'}
+    page = requests.post('http://mileage.railmiles.org/rmv2a.php/', data=query)
+
+    if "Error: " in page.text:
+        raise Exception('railmiles.org returned an error')
+
+    miles_pattern = re.compile('>Distance: (.*)mi \d')
+    miles = int(re.findall(miles_pattern, page.text)[0])
+    chains_pattern = re.compile('>Distance: \S* (.*)ch -')
+    chains = int(re.findall(chains_pattern, page.text)[0])
+    
+    dist = miles + chains * CHAINS_PER_MILE
+    
+    if units == 'km':
+        dist = dist * KM_PER_MILE
+    elif units == 'miles':
+        pass
+    else:
+        raise Exception('%s is not a valid unit system. Use "km" or "miles"' % units)
+    
+    return dist
+
+def great_circle_distance(latlng_a, latlng_b):
+    ''' From Gist https://gist.github.com/gabesmed/1826175 '''
+    lat1, lon1 = latlng_a
+    lat2, lon2 = latlng_b
+ 
+    dLat = math.radians(lat2 - lat1)
+    dLon = math.radians(lon2 - lon1)
+    a = (math.sin(dLat / 2) * math.sin(dLat / 2) +
+            math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * 
+            math.sin(dLon / 2) * math.sin(dLon / 2))
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    d = EARTH_RADIUS * c
+    
+    return d    
+
+def tests():    
+    print '1', air_distance('london euston', 'leeds') # this works
+    print '2', air_distance('liverpool lime street', 'leeds')
+    print '3', road_distance('london euston', 'leeds') # this works
+    print '4', road_distance('liverpool lime street', 'leeds')
+    print '5', rail_distance('london euston', 'leeds') # this works
+    print '6', rail_distance('liverpool lime street', 'leeds') # this throws an error 
+
+
+if __name__ == "__main__":
+    tests()
